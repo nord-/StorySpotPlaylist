90900a76a17246a243d823bda664e5197f67ed91
Rickard Nord <rickard.nord@chargenode.eu>
2021-01-27 09:13:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StorySpotPlaylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
